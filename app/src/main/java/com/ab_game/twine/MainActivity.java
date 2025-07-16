563056bfcc7c9f7fe61dedc57b8847398e1b8714
user <itworks.forwhat@gmail.com>
2025-07-16 14:37:15 +0900

    v.2

--- app/src/main/java/com/ab_game/twine/MainActivity.java
+++ app/src/main/java/com/ab_game/twine/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ab_game.twine;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,21 +9,35 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        // 액션바 숨기기
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // Immersive Fullscreen 설정 (상하단 시스템 바 숨김)
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setAllowFileAccess(true);
         webView.getSettings().setAllowFileAccessFromFileURLs(true);
         webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
 
         webView.loadUrl("file:///android_asset/index.html");
-        webView.getSettings().setDomStorageEnabled(true);            // DOM storage 활성화
-        webView.getSettings().setDatabaseEnabled(true);              // Database storage 활성화
-        webView.getSettings().setJavaScriptEnabled(true);            // 자바스크립트 활성화
-        webView.getSettings().setAllowFileAccess(true);              // 파일 접근 허용
-
-
     }
 }
